48abb0d80ab733bb7bfb5231a6f27d9223bc5b05
Yealing Tay <yetay@student.42kl.edu.my>
2023-11-22 17:51:09 +0800

    Added tests for using Request in Connection instance

--- test/Connection.cpp
+++ test/Connection.cpp
@@ -6,7 +6,7 @@
 /*   By: yetay <yetay@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 18:53:31 by yetay             #+#    #+#             */
-/*   Updated: 2023/11/21 19:04:51 by yetay            ###   ########.fr       */
+/*   Updated: 2023/11/22 17:11:38 by yetay            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,17 @@ int	main(void)
 
 	std::cout << conn.get_sockfd() << std::endl;
 	std::cout << conn.get_request() << std::endl;
+	std::cout << conn.get_request()->get_data() << std::endl;
 	std::cout << conn.get_response() << std::endl;
+
+	conn.set_sockfd(socket(AF_INET, SOCK_STREAM, 0));
+
+	std::cout << conn.get_sockfd() << std::endl;
+	std::cout << conn.get_request() << std::endl;
+	std::cout << conn.get_request()->get_data() << std::endl;
+	std::cout << conn.get_response() << std::endl;
+
+	close(conn.get_sockfd());
+
 	return (0);
 }
