7b01a89d8283c0d0a66f15102f221ddf1ea83a19
Yealing Tay <yetay@student.42kl.edu.my>
2023-11-24 14:19:02 +0800

    (refactor) Changed HTTP class, used Connection::serv_listen() static function to create all the listening Sockets

--- HTTP.cpp
+++ HTTP.cpp
@@ -22,31 +22,9 @@ void HTTP::init(const std::string path) {
 	std::cout << serv.get_server_names() << std::endl;
 
 	int				sockfd;
-	int				opt;
-	struct addrinfo	hint, *res;
+	struct addrinfo	res;
 
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd < 0)
-		throw CustomException("Socket failure: " + std::string(strerror(errno)));
-	opt = 1;
-	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)))
-		throw CustomException("Setsockopt failure: " + std::string(strerror(errno)));
-	memset(&hint, 0, sizeof(hint));
-	hint.ai_family = AF_INET;
-	hint.ai_protocol = 0;
-	hint.ai_socktype = SOCK_STREAM;
-	hint.ai_flags = AI_NUMERICSERV;
-	if (getaddrinfo(serv.get_host().c_str(), serv.get_port().c_str(), &hint, &res) < 0)
-		throw CustomException("Getaddrinfo failure: " + std::string(gai_strerror(errno)));
-	if (bind(sockfd, res->ai_addr, res->ai_addrlen) < 0)
-		throw CustomException("Bind failure: " + std::string(strerror(errno)));
-	if (listen(sockfd, 3) < 0)
-		throw CustomException("Listen failure: " + std::string(strerror(errno)));
-
-	std::cout << "Listening on port " << sockfd << "." << std::endl;
-
-	// Add socket to vector of listening-only sockets
-	Connection::listen_sockfds.push_back(sockfd);
+	sockfd = Connection::serv_listen(serv.get_host(), serv.get_port(), &res);
 
 	Poll			po;
 	int				polls;
@@ -66,7 +44,7 @@ void HTTP::init(const std::string path) {
 		std::cout << po << std::endl;
 
 		// CHECKS THROUGH ALL THE FDS IN THE POLL FDS ARRAY FOR REVENTS
-		po.process(conn, res);
+		po.process(conn, &res);
 	}
 
 };
