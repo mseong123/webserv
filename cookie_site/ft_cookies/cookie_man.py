1ae9cf8d9d0b8376270973ea686803f066ff4e68
Yealing Tay <yetay@student.42kl.edu.my>
2023-12-09 15:04:59 +0800

    (feat) changed read_dict() function to handle if DB file is not exist or empty, return empty dictionary

--- cookie_site/ft_cookies/cookie_man.py
+++ cookie_site/ft_cookies/cookie_man.py
@@ -6,12 +6,13 @@
 #    By: yetay <yetay@student.42kl.edu.my>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/12/09 12:58:04 by yetay             #+#    #+#              #
-#    Updated: 2023/12/09 14:42:48 by yetay            ###   ########.fr        #
+#    Updated: 2023/12/09 15:03:03 by yetay            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import random
 import string
+import os
 import json
 
 
@@ -23,13 +24,17 @@ def make_sid(N):
 def read_dict():
 	"""Returns a dictionary loaded from data file"""
 	cdict = None
-	with open("cookie_site/data/ctr.dict", mode="r") as file:
-		cdict = file.read()
-		if len(cdict) == 0:
-			cdict = {}
-		else:
-			cdict = json.loads(cdict)
-		file.close()
+	datafile = "cookie_site/data/ctr.dict"
+	if os.path.exists(datafile) and os.path.isfile(datafile):
+		with open("cookie_site/data/ctr.dict", mode="r") as file:
+			cdict = file.read()
+			if len(cdict) == 0:
+				cdict = {}
+			else:
+				cdict = json.loads(cdict)
+			file.close()
+	if cdict is None:
+		cdict = {}
 	return cdict
 
 
