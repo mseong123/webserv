281e86d32a475636e91f09270dedfd5a1b54f8a7
Yealing Tay <yetay@student.42kl.edu.my>
2023-12-09 14:55:10 +0800

    (feat) Added to cookie_man.py, read_dict() and write_dict() functions; also updated index.py to use these functions to read and write cookie dictionary

--- cookie_site/ft_cookies/cookie_man.py
+++ cookie_site/ft_cookies/cookie_man.py
@@ -6,14 +6,36 @@
 #    By: yetay <yetay@student.42kl.edu.my>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/12/09 12:58:04 by yetay             #+#    #+#              #
-#    Updated: 2023/12/09 14:08:41 by yetay            ###   ########.fr        #
+#    Updated: 2023/12/09 14:42:48 by yetay            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import random
 import string
+import json
 
 
 def make_sid(N):
 	"""Returns a randomly generated alphanumeric string"""
 	return "".join(random.choices(string.ascii_letters + string.digits, k=N))
+
+
+def read_dict():
+	"""Returns a dictionary loaded from data file"""
+	cdict = None
+	with open("cookie_site/data/ctr.dict", mode="r") as file:
+		cdict = file.read()
+		if len(cdict) == 0:
+			cdict = {}
+		else:
+			cdict = json.loads(cdict)
+		file.close()
+	return cdict
+
+
+def write_dict(cdict):
+	"""Write a dictionary to data file"""
+	with open("cookie_site/data/ctr.dict", mode="w") as file:
+		file.write(json.dumps(cdict))
+		file.close()
+	return True
--- cookie_site/index.py
+++ cookie_site/index.py
@@ -4,7 +4,7 @@ import cgi
 import os
 import sys
 import cgitb
-from ft_cookies.cookie_man import make_sid
+from ft_cookies.cookie_man import make_sid, read_dict, write_dict
 
 
 cgitb.enable()
@@ -13,9 +13,22 @@ form = cgi.FieldStorage()
 request_method = os.environ.get("REQUEST_METHOD").upper()
 
 if request_method == "GET":
+	#### Read cookie database
+	cdict = read_dict()
+	
+	if os.environ.get("COOKIE") is None:
+		sid = make_sid(16)
+		while sid in cdict:
+			sid = make_sid(16)
+		cdict[sid] = str(1)
+	write_dict(cdict)
+	
+	#### Create response HTTP header
 	print("HTTP/1.1 200 OK")
 	print("Content-Type: text/html")
 	print("\r\n")
+
+	#### Create response HTTP body
 	with open("cookie_site/data/html/head", mode="r") as file:
 		file_content = file.read()
 		file.close()
@@ -26,5 +39,4 @@ if request_method == "GET":
 		file_content = file.read()
 		file.close()
 	print(file_content)
-	print(make_sid(16))
 	print("\r\n")
